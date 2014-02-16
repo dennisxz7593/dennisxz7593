09f500a5cd65f5840726c860f65851ef738e5b44
dennisxz <sanjose.dennis@gmail.com>
2014-02-16 06:56:13 -0800

    Create ScavHunt.java

---
+++ ScavHunt.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scavhunt;
+
+/**
+ *
+ * @author sanjose
+ */
+public class ScavHunt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ScavHunt1 sh1 = new ScavHunt1();
+       sh1.show();
+    }
+}
